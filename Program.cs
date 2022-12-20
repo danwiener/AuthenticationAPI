c5c37afc14a31589ffbd1e8031c46bde73e305f0
Henrik Wiener <henrik.wiener@outlook.com>
2022-12-20 15:12:33 -0800

    Ignored

--- Program.cs
+++ Program.cs
@@ -1,10 +1,13 @@
 using Authentication.Data;
 using Authentication.Models;
 using Authentication.Services;
+using Microsoft.AspNetCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 
+
 var builder = WebApplication.CreateBuilder(args);
+
 
 // Add services to the container.
 
@@ -12,24 +15,24 @@ builder.Services.AddControllers();
 
 builder.Services.AddDbContext<FFContextDb>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+
+
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 
-app.UseCors(options => options.WithOrigins(new[] { "http://localhost:3000", "http://localhost:8080", "http://localhost:4200" }).AllowAnyHeader().AllowAnyMethod().AllowCredentials());
+app.UseCors(options => options.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
 
 app.UseAuthorization();
 app.MapControllers();
 
 
-if (app.Environment.IsDevelopment())
-{
-    app.Run();
-}
-else
-{
-    app.Run("http://localhost:8000");
-}
+
+app.Run();
+
+
 
 //using var db = new FFContextDb();
 //var user = db.Users
